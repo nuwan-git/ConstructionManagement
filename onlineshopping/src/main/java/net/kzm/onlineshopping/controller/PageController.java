3c31ed32558cb17cff0d34adb0012cbb0e700965
nuwan-git <nuwanrathnayaka.c@gmail.com>
2018-01-06 02:21:33 +0530

    Successfully loaded static resources

--- onlineshopping/src/main/java/net/kzm/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzm/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.kzm.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -17,6 +19,33 @@ public class PageController {
 		return mv;
 		
 	}
+//	@RequestMapping (value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting_ ){
+//		if(greeting_==null){
+//			greeting_="Hello there";
+//		}
+//		
+//		
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("msg",greeting_);
+//		System.out.println(mv);
+//		return mv;
+//	}
+	
+/*	@RequestMapping (value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting_ ){
+		if(greeting_==null){
+			greeting_="Hello there";
+		}
+		
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("msg",greeting_);
+		System.out.println(mv);
+		return mv;
+	}*/
+	
+	
 	
 	
 }
